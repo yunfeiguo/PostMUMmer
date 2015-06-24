351563976ab204b700d2f246e2204a888549d07b
Yunfei Guo <yunfeigu@usc.edu>
2015-06-24 15:16:04 -0700

    add code for output BED for mapped query

--- hpc/proc_coords_with_BEDmerging.pl
+++ hpc/proc_coords_with_BEDmerging.pl
@@ -21,35 +21,35 @@ my %fa = &SeqMule::Utils::readFastaIdx($idx);
 my $total = scalar keys %fa;
 my @cleanQ; #files to be removed
 for my $i(@ARGV) {
-    open IN,'<',$i or die "open($i): $!\n";
-    while(<IN>){
-	s/\|//g;
-	s/^[\t ]+//;
-	next if /^[=\/\s\[]|^NUCMER/;
-	chomp;
-	my @f=split;
-	die "ERROR: expected 13 fields: $_\n" unless @f==13;
-	warn "DEBUG:@f\n" if $debug;
-	#here we only care how much of the query is aligned, regardless of alignment location
-	my $query_start = $f[2];
-	my $query_end = $f[3];
-	my $ref_start = $f[0];
-	my $ref_end = $f[1];
-	my $id = $f[12];
-	my $chr = $f[11];
-	($query_start,$query_end) = &smallerFirst($query_start,$query_end);
-	($ref_start,$ref_end) = &smallerFirst($ref_start,$ref_end);
-	#we need to make sure all regions that are accepted as final mapped regions come from
-	#a continuous sequence on the chr
-	if(defined $fa{$id}->{queryMapping} ) {
-	    push @{$fa{$id}->{refMapping}},[$chr,$ref_start,$ref_end];
-	    push @{$fa{$id}->{queryMapping}},[$chr,$query_start,$query_end];
-	} else {
-	    $fa{$id}->{refMapping} = [[$chr,$ref_start,$ref_end]];
-	    $fa{$id}->{queryMapping} = [[$chr,$query_start,$query_end]];
+	open IN,'<',$i or die "open($i): $!\n";
+	while(<IN>){
+		s/\|//g;
+		s/^[\t ]+//;
+		next if /^[=\/\s\[]|^NUCMER/;
+		chomp;
+		my @f=split;
+		die "ERROR: expected 13 fields: $_\n" unless @f==13;
+		warn "DEBUG:@f\n" if $debug;
+		#here we only care how much of the query is aligned, regardless of alignment location
+		my $query_start = $f[2];
+		my $query_end = $f[3];
+		my $ref_start = $f[0];
+		my $ref_end = $f[1];
+		my $id = $f[12];
+		my $chr = $f[11];
+		($query_start,$query_end) = &smallerFirst($query_start,$query_end);
+		($ref_start,$ref_end) = &smallerFirst($ref_start,$ref_end);
+		#we need to make sure all regions that are accepted as final mapped regions come from
+		#a continuous sequence on the chr
+		if(defined $fa{$id}->{queryMapping} ) {
+			push @{$fa{$id}->{refMapping}},[$chr,$ref_start,$ref_end];
+			push @{$fa{$id}->{queryMapping}},[$chr,$query_start,$query_end];
+		} else {
+			$fa{$id}->{refMapping} = [[$chr,$ref_start,$ref_end]];
+			$fa{$id}->{queryMapping} = [[$chr,$query_start,$query_end]];
+		}
 	}
-    }
-    close IN;
+	close IN;
 }
 warn "reading coords done\n";
 &convert2BED(\%fa);
@@ -62,126 +62,136 @@ warn "All done\n";
 
 ###################################################################
 sub convert2BED {
-    my $ref = shift;
-    my $count = 0;
-    for my $i(keys %$ref) {
-	$count++;
-	warn "conversion:$count/$total done\n" if $count % 100 == 0;
-	next unless defined $ref->{$i}->{refMapping};
-	#the mapping may contain overlapping regions
-	#we should only use non-overlapped regions
-	my $ref_bed = "$tmpdir/$i".rand($$).".ref.bed";
-	my $ref_overlapBED = &SeqMule::Utils::genBED($ref->{$i}->{refMapping});
-	$ref->{$i}->{refBED} = &mergeBED($ref_overlapBED,0,$ref_bed);
-	push @cleanQ,$ref_overlapBED,$ref_bed;
-    }
+	my $ref = shift;
+	my $count = 0;
+	for my $i(keys %$ref) {
+		$count++;
+		warn "conversion:$count/$total done\n" if $count % 100 == 0;
+		next unless defined $ref->{$i}->{refMapping};
+		#the mapping may contain overlapping regions
+		#we should only use non-overlapped regions
+		my $ref_bed = "$tmpdir/$i".rand($$).".ref.bed";
+		my $ref_overlapBED = &SeqMule::Utils::genBED($ref->{$i}->{refMapping});
+		$ref->{$i}->{refBED} = &mergeBED($ref_overlapBED,0,$ref_bed);
+		push @cleanQ,$ref_overlapBED,$ref_bed;
+	}
 }
 sub mergeBED_extractMaxMapping {
-    my $ref = shift;
-    my $count = 0;
-    for my $i(keys %$ref) {
-	$count++;
-	warn "merge and extractiong: $count/$total done\n" if $count % 100 == 0;
-	next unless defined $ref->{$i}->{refBED};
-	&findMapping($ref->{$i});
-    }
+	my $ref = shift;
+	my $count = 0;
+	for my $i(keys %$ref) {
+		$count++;
+		warn "merge and extraction: $count/$total done\n" if $count % 100 == 0;
+		next unless defined $ref->{$i}->{refBED};
+		&findMapping($ref->{$i});
+	}
 }
 sub findMapping {
-    my $query_ref = shift;
-    my $ref_bed = $query_ref->{refBED};
-    my $out = &mergeBED($ref_bed,$gap); #merge with gap allowance
-    push @cleanQ,$out;
-    #maxMapping = [chr,start,end]
-    $query_ref->{maxMapping} = &findMax([&SeqMule::Utils::readBED($out)]);
-    $query_ref->{mappedLen} = &getMappedLen($query_ref->{maxMapping},$query_ref->{refMapping},$query_ref->{queryMapping});
+	my $query_ref = shift;
+	my $ref_bed = $query_ref->{refBED};
+	my $out = &mergeBED($ref_bed,$gap); #merge with gap allowance
+	push @cleanQ,$out;
+	#maxMapping = [chr,start,end]
+	$query_ref->{maxMapping} = &findMax([&SeqMule::Utils::readBED($out)]);
+	($query_ref->{mappedLen},$query_ref->{mappedQueryBed}) = &getMappedLen($query_ref->{maxMapping},$query_ref->{refMapping},$query_ref->{queryMapping});
 }
 sub getMappedLen {
-    #based on maxMapping found on ref
-    #identify mapped regions on query
-    #get total length of mapped regions on
-    #query
-    my $max = shift;
-    my $allRef = shift;
-    my $allQuery = shift;
-    my $chr = $max->[0];
-    my $start = $max->[1];
-    my $end = $max->[2];
-    my $mappedLen = 0;
-    my @mappedQuery;
-    for my $i(0..$#{$allRef}) {
-	#the mapped region must be fully contained
-	#as we have merged all mapped regions
-	next unless $allRef->[$i]->[0] eq $chr and $allRef->[$i]->[1] >= $start and $allRef->[$i]->[2] <= $end;
-	push @mappedQuery,$allQuery->[$i];
-    }
-    #mapped regions in query should be merged before
-    #length is calculated
-    my $query_mapped_bed = &SeqMule::Utils::genBED(\@mappedQuery);
-    my $query_mapped_nonoverlap_bed = &mergeBED($query_mapped_bed);
-    $mappedLen = &SeqMule::Utils::bed2total($query_mapped_nonoverlap_bed);
+	#based on maxMapping found on ref
+	#identify mapped regions on query
+	#get total length of mapped regions on
+	#query
+	my $max = shift;
+	my $allRef = shift;
+	my $allQuery = shift;
+	my $chr = $max->[0];
+	my $start = $max->[1];
+	my $end = $max->[2];
+	my $mappedLen = 0;
+	my @mappedQuery;
+	for my $i(0..$#{$allRef}) {
+		#the mapped region must be fully contained
+		#as we have merged all mapped regions
+		next unless $allRef->[$i]->[0] eq $chr and $allRef->[$i]->[1] >= $start and $allRef->[$i]->[2] <= $end;
+		push @mappedQuery,$allQuery->[$i];
+	}
+	#mapped regions in query should be merged with gap of 0 before
+	#length is calculated
+	my $query_mapped_bed = &SeqMule::Utils::genBED(\@mappedQuery);
+	my $query_mapped_nonoverlap_bed = &mergeBED($query_mapped_bed);
+	$mappedLen = &SeqMule::Utils::bed2total($query_mapped_nonoverlap_bed);
 
-    push @cleanQ,$query_mapped_bed,$query_mapped_nonoverlap_bed;
-    return $mappedLen;
+	push @cleanQ,$query_mapped_bed,$query_mapped_nonoverlap_bed;
+	return ($mappedLen,$query_mapped_nonoverlap_bed);
 }
 sub findMax {
-    #given a set of regions in BED format
-    #find the longest the region
-    my $ref = shift;
-    my $max = 0;
-    my $pos = [];
-    #array of [chr,start,end]
-    for my $i(@$ref) {
-	my $len = $i->[2]-$i->[1];
-	#if there are multiple alignments with equal lengths,
-	#we output first one of them
-	if ($len > $max) {
-	    $pos = $i;
-	    $max = $len;
+	#given a set of regions in BED format
+	#find the longest the region
+	my $ref = shift;
+	my $max = 0;
+	my $pos = [];
+	#array of [chr,start,end]
+	for my $i(@$ref) {
+		my $len = $i->[2]-$i->[1];
+		#if there are multiple alignments with equal lengths,
+		#we output first one of them
+		if ($len > $max) {
+			$pos = $i;
+			$max = $len;
+		}
 	}
-    }
-    return $pos;
+	return $pos;
 }
 sub output {
-    my $ref = shift;
-    my %fa = %$ref;
-    print join("\t","FASTA_ID","Mapped_length","Total_length","Mapping_ratio","Mapped_chr","Query_start","Query_end"),"\n";
-    for my $i(keys %fa) {
-	my $mappedLen = $fa{$i}->{mappedLen} || 0;
-	my $totalLen = $fa{$i}->{length};
-	my $mapping = ['NA','NA','NA'];
-	if(defined $fa{$i}->{maxMapping}) {
-	    $mapping = $fa{$i}->{maxMapping};
+	my $ref = shift;
+	my $outputDir = "mapped_query_bed";
+	mkdir $outputDir unless -d $outputDir;
+	warn "mapped regions in query will be saved to $outputDir in BED format.\n";
+	my %fa = %$ref;
+	print join("\t","FASTA_ID","Mapped_length","Total_length","Mapping_ratio","Mapped_chr","Query_start","Query_end"),"\n";
+	for my $i(keys %fa) {
+		my $mappedLen = $fa{$i}->{mappedLen} || 0;
+		my $totalLen = $fa{$i}->{length};
+		my $mapping = ['NA','NA','NA'];
+		if(defined $fa{$i}->{maxMapping}) {
+			$mapping = $fa{$i}->{maxMapping};
+		}
+		my $mappingRatio = $totalLen == 0? 0:$mappedLen/$totalLen;
+		#output mapping stats
+		print join("\t",$i,
+			$mappedLen,
+			$totalLen,
+			sprintf("%.2f",$mappingRatio),
+			@$mapping,
+		), "\n";
+		#write mapped regions of query in BED format
+		#we need to replace chr name by query contig name
+		#because previously we put chr name here to differentiate 
+		#alignments on different chromosomes.
+		!system("perl -pe 's/^(\\S+)/$i/' ".$fa{$i}->{mappedQueryBed}." > $outputDir/$i.mappedQuery.bed")
+		 or die "failed to copy BED $i: $!\n" if defined $fa{$i}->{mappedQueryBed};
 	}
-	my $mappingRatio = $totalLen == 0? 0:$mappedLen/$totalLen;
-	print join("\t",$i,
-	    $mappedLen,
-	    $totalLen,
-	    sprintf("%.2f",$mappingRatio),
-	    @$mapping,
-	), "\n";
-    }
 }
 sub cleanup {
-    #remove temp files
-    unlink @cleanQ;
+	#remove temp files
+	unlink @cleanQ;
 }
 sub smallerFirst {
-    my $start = shift;
-    my $end = shift;
-    if($start > $end) {
-	#make sure end is no smaller than start
-	$start = $end + $start;
-	$end = $start - $end;
-	$start = $start - $end;
-    }
-    return($start,$end);
+	my $start = shift;
+	my $end = shift;
+	if($start > $end) {
+		#make sure end is no smaller than start
+		$start = $end + $start;
+		$end = $start - $end;
+		$start = $start - $end;
+	}
+	return($start,$end);
 }
 sub mergeBED {
-    my $bed = shift;
-    my $gap = shift;
-    my $out = shift;
-    $gap = $gap || 0;
-    $out = $out || "$tmpdir/$$".rand($$).".tmp.bed";
-    !system("$bedtools sort -i $bed | $bedtools merge -d $gap > $out") or die "merging $bed fail: $!\n";
-    return $out;
+	my $bed = shift;
+	my $gap = shift;
+	my $out = shift;
+	$gap = $gap || 0;
+	$out = $out || "$tmpdir/$$".rand($$).".tmp.bed";
+	!system("$bedtools sort -i $bed | $bedtools merge -d $gap > $out") or die "merging $bed fail: $!\n";
+	return $out;
 }
