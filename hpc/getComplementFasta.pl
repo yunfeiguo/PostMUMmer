8079c3648287606a434f526441e372be934b043e
Yunfei Guo <yunfeigu@usc.edu>
2015-07-06 10:31:06 -0700

    add genome file generation function

--- hpc/getComplementFasta.pl
+++ hpc/getComplementFasta.pl
@@ -3,14 +3,17 @@
 use strict;
 use warnings;
 use File::Basename qw/basename/;
+my $minLen = 10_000; #min length for unmapped region
 
-die "Usage: $0 <bedtools> <bed> <genome length file> <genome fasta>\n" unless @ARGV == 4;
+die "Usage: $0 <bedtools> <bed> <indexed genome fasta>\n" unless @ARGV == 3;
 my $bedtools = shift;
 my $bed = shift;
-my $genome = shift;
+my $genome = "/tmp/".rand($$).".genome";
 my $fa = shift;
-my $outFa = basename $bed.".complement.fasta";
-my $complementBED = basename $bed.".complement.bed";
+my $fai = "$fa.fai";
+my $outFa = basename $bed.".complement.$minLen.fasta";
+my $complementBED = basename $bed.".complement.$minLen.bed";
 
-!system("$bedtools sort -i $bed | $bedtools complement -i $bed -g $genome > $complementBED") or die "bedtools complement: $!\n";
+!system("cut -f 1,2 $fai > $genome") or die "cut $fai: $!\n";
+!system("$bedtools sort -i $bed | $bedtools complement -i $bed -g $genome | perl -ne '\@f=split;print if \$f[2]-\$f[1] >= $minLen' > $complementBED") or die "bedtools complement: $!\n";
 !system("$bedtools getfasta -fi $fa -bed $complementBED -fo $outFa") or die "bedtools getfasta: $!\n";
